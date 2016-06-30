78c10ec1958fe72a676427471c5738dc791ec939
cht527 <cht527@126.com>
2016-06-30 22:12:51 +0800

    change graduate design

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-g++ `pkg-config opencv --libs --cflags opencv` FishEye.cpp -o FishEye
-#g++  `pkg-config --cflags opencv` -o FishEye.cpp `pkg-config --libs opencv`
-./FishEye /var/www/ffmpeg/ffmpeg/yuyan.jpg
-
-
